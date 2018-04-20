553f7cbb21943e3f7784fe0b4393ac651b404e95
brianclauzel <brianthomasclauzel@gmail.com>
2018-04-19 21:10:17 -0700

    added functionality

--- bamazon.sql
+++ bamazon.sql
@@ -7,7 +7,8 @@ CREATE TABLE products (
 	product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR (100) NOT NULL,
     price INTEGER NOT NULL,
-	stock_quantity INTEGER NOT NULL
+	stock_quantity INTEGER NOT NULL,
+    PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values (10, 'shower curtain', 'home', 22.99, 4);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@ var mySQL = require('mysql');
 var inquirer = require('inquirer');
 var connection = mySQL.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: 'Shredder321@',
     database: 'bamazon'
@@ -9,17 +10,96 @@ var connection = mySQL.createConnection({
 
 
 connection.connect(function(err){
+
     if (err){
         console.log(err);
     }
+
+    firstQuestion();
+
+    // connection.end();
 });
 
+//test to see all of inventory and thread id
+function afterConnection() {
 connection.query('SELECT * FROM products', function(error, results, fields) {
-    if (error) {
-        console.log(error);
-    }
-    for (i = 0; i < results.length; i++) {
-        console.log(results[i] + "\n");
-    }
     
-});
+        if (error) {
+            console.log(error);
+        }
+        
+        console.log("id: " + connection.threadId);
+
+        for (i = 0; i < results.length; i++) {
+            console.log(results[i].product_name);
+        }
+
+        connection.end();
+    });
+
+};
+
+function firstQuestion() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the I.D. number of the item you would like to purchase",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "How many of this item would you like to purchase?",
+            name: "quantity"
+        }
+    ]).then(function(answer) {
+
+        connection.query("SELECT * FROM products", function(error, results) {
+
+            var chosenId;
+            var isNotFound = true;
+
+            if (error) {
+                 console.log(error);
+            }
+
+            for (var i = 0; i < results.length; i++) {
+                
+                if (results[i].item_id === Number(answer.id) && results[i].stock_quantity >= Number(answer.quantity)) {
+                    chosenId = results[i];
+                    console.log(results[i].product_name + ", yes we have that in stock");
+                    isNotFound = false;
+                    productDetails(results[i]);
+                }
+
+                else if ((results.length - 1) === i && isNotFound)
+                {
+                    console.log("sorry we dont have that product");
+                }
+            }
+            connection.end();
+        });
+    });
+}
+
+function secondQuestion() {
+    inquier.prompt();
+}
+
+
+function productDetails(chosenItem) {
+    connection.query("SELECT * FROM products", function(error, results) {
+
+        console.log(chosenItem.product_name +  " costs: " + chosenItem.price);
+
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Would you like to purchase this item?",
+                name: "answer"
+            }
+        ]).then(function(error, answer) {
+
+        });
+        
+    });
+}
